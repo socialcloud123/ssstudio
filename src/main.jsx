18f23a9cf66b0f56d5476d4760a8a52a1b3d0b61
GSD Agent <gsd-agent@example.com>
2026-03-06 12:31:43 +0530

    speed insights vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App'
 import './index.css'
 
@@ -7,5 +8,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
